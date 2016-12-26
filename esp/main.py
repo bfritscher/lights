5c0743a6a13330dfb7f4c626d1ff498584722254
Boris Fritscher <boris@fritscher.ch>
2016-12-26 20:55:06 +0100

    delta timeout only if needed

--- esp/main.py
+++ esp/main.py
@@ -11,6 +11,9 @@ sf.test()
 w = Color(255, 255, 255)
 off = Color(0, 0, 0)
 
+def wait(ms):
+   time.sleep(ms/1000.0)
+
 animation = "sf.color(w)"
 
 def sub_cb(topic, msg):
@@ -22,21 +25,26 @@ def sub_cb(topic, msg):
         pass
 
 
+last_anim = time.ticks_ms()
+
+c = MQTTClient("umqtt_client", MQTT_BROKER)
+c.set_callback(sub_cb)
 
 
 def play_animation():
-    global animation
+    global animation, last_anim
     try:
         exec(animation, globals(), locals())
     except Exception as e:
         print(e)
+        c.publish(b"lights/snowflake/error", b"%s" % e)
         animation = "sf.color(Color(255, 0, 0))"
-    time.sleep(500/1000.0)
-
+    delta = time.ticks_ms() - last_anim
+    if delta < 500:
+        time.sleep((500 - delta) / 1000.0)
+    last_anim = time.ticks_ms()
 
 def main():
-    c = MQTTClient("umqtt_client", MQTT_BROKER)
-    c.set_callback(sub_cb)
     c.connect()
     c.subscribe(b"lights/snowflake")
     while True:
