b27b60070426b2adfe3bac39f13f5c67ba5ce64a
Boris Fritscher <boris@fritscher.ch>
2016-12-26 23:17:44 +0100

    colorpicker rgb helper

--- app/scripts/controllers/snowflake.js
+++ app/scripts/controllers/snowflake.js
@@ -14,6 +14,25 @@ angular.module('lightsApp')
       lineNumbers: true,
     };
 
+    $scope.colorPickerOptions = {
+        preferredFormat: 'rgb',
+        showInput: true,
+        containerClassName: 'colorpicker',
+        flat: true,
+        showInitial: false,
+        localStorageKey: 'colorpicker',
+        showPalette: true,
+        showSelectionPalette: true,
+        chooseText: 'save to palette',
+        cancelText: '',
+        palette: [ '#001F3F', '#0074D9', '#7FDBFF', '#39CCCC',
+                   '#3D9970', '#2ECC40', '#01FF70', '#FFDC00',
+                   '#FF851B', '#FF4136', '#F012BE', '#B10DC9',
+                   '#85144B', '#FFFFFF', '#AAAAAA', '#DDDDDD',
+                   '#111111'
+        ]
+      };
+
     $scope.animation = 'sf.color(Color(0,0,255))\n'
       + 'wait(500)\n'
       + 'sf.color(Color(0,255,0))\n'
