953dfce6dfb0ee89d453eca4c34f4b7d29eee79a
Boris Fritscher <boris@fritscher.ch>
2016-12-26 22:30:06 +0100

    color helper function

--- esp/main.py
+++ esp/main.py
@@ -14,6 +14,17 @@ off = Color(0, 0, 0)
 def wait(ms):
    time.sleep(ms/1000.0)
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
 animation = "sf.color(w)"
 
 def sub_cb(topic, msg):
